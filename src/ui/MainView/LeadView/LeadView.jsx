b70101cb988de98d52ee6cddf482cc49bb414ffe
santiago arteche <santiarteche@hotmail.com>
2024-07-16 11:36:30 -0300

    button main fixed

--- src/ui/MainView/LeadView/LeadView.jsx
+++ src/ui/MainView/LeadView/LeadView.jsx
@@ -4,12 +4,12 @@ import { useContext } from "react";
 import { LeadContext } from "../../../context/LeadContext/LeadContext";
 
 export const LeadView = () => {
-  const { lead, handleLead } = useContext(LeadContext);
+  const { lead } = useContext(LeadContext);
 
   return (
     <div className="col-span-7 p-5 max-h-[780px] overflow-auto">
       <div className="flex gap-3 text-gray-500">
-        <IoIosArrowUp onClick={() => handleLead()} />
+        <IoIosArrowUp />
         <IoIosArrowDown />
       </div>
       <h2 className="text-4xl mt-10 font-semibold hover:bg-blue-100 p-3 hover:rounded cursor-pointer">
